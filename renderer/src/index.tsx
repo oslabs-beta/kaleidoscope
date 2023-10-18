55728acf6d1914e81b5020d72a2896ca9e164787
Herman <hermanchen77@gmail.com>
2023-10-18 15:05:48 -0400

    react-router

--- renderer/src/index.tsx
+++ renderer/src/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import NodeMap from './components/NodeMap';
+import ViewCluster from './components/ViewCluster';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/viewlogin",
+    element: <NodeMap />,
+  },
+  {
+    path: "/viewlogin",
+    element: <ViewCluster />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <RouterProvider router={router} />
 );
