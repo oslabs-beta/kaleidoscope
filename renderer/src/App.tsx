b83f4250bc34fad9a207e7ad94f858dcf082fed1
ChrisJettoo <cjettoostudent@gmail.com>
2023-10-21 10:19:52 -0400

    updated node map

--- renderer/src/App.tsx
+++ renderer/src/App.tsx
@@ -3,16 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import ViewCluster from './components/ViewCluster';
 
-// const App = () => {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/viewCluster" element={<ViewCluster />} />
-//       </Routes>
-//     </Router>
-//   );
-// };
 
 function App() {
 
