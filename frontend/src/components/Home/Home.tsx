90f9738e9f25fd08c2da0c17c27244e830e54bf3
Rob Mayo <rbrtm984@gmail.com>
2023-10-23 17:15:40 -0400

    finished styling Home page using basic tailwind styles

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -20,36 +20,77 @@ const Home = () => {
     return (
         <div className="flex flex-col items-center h-screen justify-center bg-gray-100">
             <h1 className="text-4x1 font-semibold mb-8">Kaleidoscope</h1>
-            <div>
-                <label htmlFor="username">AWS Access Key ID:</label>
+            <div className="mb-4">
+                <label 
+                htmlFor="username" 
+                className="block text-sm font-medium text-gray-700"
+                >
+                    AWS Access Key ID:
+                </label>
                 <input
                 type="text"
                 id="username"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
+                className="my-1 p-2 w-full rounded-md border"
                 />
             </div>
-            <div>
-                <label htmlFor="password">AWS Secret Access Key:</label>
+            <div className="mb-4">
+                <label 
+                htmlFor="password" 
+                className="block text-sm font-medium text-gray-700"
+                >
+                    AWS Secret Access Key:
+                </label>
                 <input
                 type="password"
                 id="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
+                className="my-1 p-2 w-full rounded-md border"
             />
             </div>
-            <div>
-                <label htmlFor="password">Cluster Name:</label>
-                <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <div className="mb-4">
+                <label 
+                htmlFor="password" 
+                className="block text-sm font-medium text-gray-700"
+                >
+                    Cluster Name:
+                </label>
+                <input 
+                type="password" 
+                id="password" 
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)} 
+                className="my-1 p-2 w-full rounded-md border"
+                />
             </div>
-            <div>
-                <label htmlFor="password">Region:</label>
-                <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <div className="mb-4">
+                <label 
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+                >
+                    Region:
+                </label>
+                <input 
+                type="password" 
+                id="password" 
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)} 
+                className="my-1 p-2 w-full rounded-md border"
+                />
             </div>
-            <button onClick={handleAWSLogin}>AWS Login</button>
-                <Link to="/viewlogin">
-                    <button>View Your Local Cluster</button>
+            <div className="mt-6">
+                <button 
+                onClick={handleAWSLogin} 
+                className="px-4 py-2 bg-blue-500 text-white rounded-md"
+                >
+                    AWS Login
+                </button>
+                <Link to="/viewlogin" className="ml-4">
+                    <button className="px-4 py-2 bg-green-500 text-white rounded-md">View Your Local Cluster</button>
                 </Link>
+            </div>
         </div>
     );
 };
