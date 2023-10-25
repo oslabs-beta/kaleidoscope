064fedc29599324c1897dac50d5fb5282f06219b
Rob Mayo <rbrtm984@gmail.com>
2023-10-25 17:10:09 -0400

    continued refactoring nodemap. changed circles to be state.

--- backend/routers/nodemapRouter.ts
+++ backend/routers/nodemapRouter.ts
@@ -4,31 +4,7 @@ const nodemapController = require('../controllers/nodemapController.ts');
 const router = Router()
 
 router.get('*', nodemapController.getSpans, nodemapController.makeNodes, (Request, Response) => {
-    // console.log('nodemaprouter hitttttt')
-    console.log('res.locals.nodes', Response.locals.nodes)
     Response.status(203).json(Response.locals.nodes);
 });
 
 module.exports = router;
-
-
-
-
-
-
-
-
-
-// const express = require('express');
-// const { req: Request, res: Response} = require('express')
-// const nodemapController = require('./controllers/nodemapController')
-// const nodemapRouter = express.Router();
-
-
-// nodemapRouter.get('*', nodemapController.getSpans, (req, res) => {
-//     res.status(200).send(res.locals.spans);
-// });
-
-
-
-// export = nodemapRouter;
