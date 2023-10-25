3a742630d40a60d26d65ea736f8a88a31ae54af0
Rob Mayo <rbrtm984@gmail.com>
2023-10-25 14:58:46 -0400

    started breaking up and modularizing nodemap component. moving makenodes component to the backend folder. moved types to types file. moving draw to its own file

--- frontend/src/components/NodeMap/NodeMap.tsx
+++ frontend/src/components/NodeMap/NodeMap.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { AnnotationForm } from '../AnnotationForm/AnnotationForm';
 import { AnnotationMenu } from '../AnnotationMenu/AnnotationMenu';
 import { Circle, Line, Span } from '../../types';
+import { draw } from './draw';
 
 const makeNodes = async () => {
     // console.log('invoked awaitFunc');
@@ -104,68 +105,12 @@ export default function NodeMap() {
     // Function to toggle annotation menu
     const toggleAnnotationMenu = () => setShowAnnotationMenu(!showAnnotationMenu);
 
-    // Function to draw circle on canvas
-    const drawCircle = (canvasContext, circle) => {
-        canvasContext.beginPath();
-        canvasContext.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2);
-        canvasContext.fillStyle = 'black';
-        canvasContext.fill();
-        canvasContext.closePath();
-    };
-
-    // Function to draw line between circles
-    const drawLine = (canvasContext, circleA, circleB) => {
-        canvasContext.beginPath();
-        canvasContext.moveTo(circleA.x, circleA.y);
-        canvasContext.lineTo(circleB.x, circleB.y);
-        canvasContext.strokeStyle = 'dark-gray';
-        canvasContext.lineWidth = 2;
-        canvasContext.stroke();
-    };
-
-    const draw = (canvasContext, canvas, circles:Circle[], lines:Line[]):void | null => {
-        // console.log('draw')
-        // clear canvas
-        canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-
-        // Draw lines with labels
-        lines.forEach(line => {
-            // console.log('Check here', circles.find(circle => circle.name === line.from)) 
-            console.log('testing', circles.find(circle => circle.name === line.from))
-            const fromCircle: Circle = circles.find(circle => circle.name === line.from);
-            const toCircle: Circle = circles.find(circle => circle.name === line.to);
-            console.log('from', fromCircle, '  to', toCircle);
-            // Draw line
-            drawLine(canvasContext, fromCircle, toCircle);
-
-        // Calculate the midpoint of the line for label positioning
-        const labelX = (fromCircle.x + toCircle.x) / 2;
-        const labelY = (fromCircle.y + toCircle.y) / 2;
-
-            // Display label
-            canvasContext.font = '12px Arial';
-            canvasContext.fillStyle = 'red';
-            canvasContext.fillText(`average latency: ${line.latency}ms requests:${line.requests}`, labelX, labelY);
-        });
-
-        // Draw circles and node labels
-        circles.forEach(circle => {
-            // call helper func
-            drawCircle(canvasContext, circle);
-            // Display trace data on the circle
-            // needs to be reconfigured w/ store
-            canvasContext.font = '12px Arial';
-            canvasContext.fillStyle = 'white';
-            canvasContext.fillText(circle.name, circle.x - 15, circle.y);
-        });
-    };
-
         /* ------------------------------ The useEffect Zone------------------------------ */
 
     // Makes map w/ new nodes and lines
     useEffect(() => {
         const getNewNodeMap = async () => {
-            const [newCircles, newLines]:any = await makeNodes(); 
+            const [newCircles, newLines]:any = await makeNodes(); // fetch goes here
             console.log('got EM', newCircles) 
             lines = newLines; 
             circles = newCircles
---
+++ frontend/src/components/NodeMap/draw.tsx
@@ -0,0 +1,57 @@
+import { Circle, Line } from '../../types';
+
+// Function to draw circle on canvas
+export const drawCircle = (canvasContext, circle) => {
+    canvasContext.beginPath();
+    canvasContext.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2);
+    canvasContext.fillStyle = 'black';
+    canvasContext.fill();
+    canvasContext.closePath();
+};
+
+// Function to draw line between circles
+export const drawLine = (canvasContext, circleA, circleB) => {
+    canvasContext.beginPath();
+    canvasContext.moveTo(circleA.x, circleA.y);
+    canvasContext.lineTo(circleB.x, circleB.y);
+    canvasContext.strokeStyle = 'dark-gray';
+    canvasContext.lineWidth = 2;
+    canvasContext.stroke();
+};
+
+export const draw = (canvasContext, canvas, circles:Circle[], lines:Line[]):void | null => {
+    // console.log('draw')
+    // clear canvas
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw lines with labels
+    lines.forEach(line => {
+        // console.log('Check here', circles.find(circle => circle.name === line.from)) 
+        console.log('testing', circles.find(circle => circle.name === line.from))
+        const fromCircle: Circle = circles.find(circle => circle.name === line.from);
+        const toCircle: Circle = circles.find(circle => circle.name === line.to);
+        console.log('from', fromCircle, '  to', toCircle);
+        // Draw line
+        drawLine(canvasContext, fromCircle, toCircle);
+
+    // Calculate the midpoint of the line for label positioning
+    const labelX = (fromCircle.x + toCircle.x) / 2;
+    const labelY = (fromCircle.y + toCircle.y) / 2;
+
+        // Display label
+        canvasContext.font = '12px Arial';
+        canvasContext.fillStyle = 'red';
+        canvasContext.fillText(`average latency: ${line.latency}ms requests:${line.requests}`, labelX, labelY);
+    });
+
+    // Draw circles and node labels
+    circles.forEach(circle => {
+        // call helper func
+        drawCircle(canvasContext, circle);
+        // Display trace data on the circle
+        // needs to be reconfigured w/ store
+        canvasContext.font = '12px Arial';
+        canvasContext.fillStyle = 'white';
+        canvasContext.fillText(circle.name, circle.x - 15, circle.y);
+    });
+};
