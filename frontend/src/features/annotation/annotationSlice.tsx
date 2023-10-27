4c6dead6cb72085f305a23fa6f7f0b9c2b167e6b
Rob Mayo <rbrtm984@gmail.com>
2023-10-27 16:14:28 -0400

    annotation menu now receives data from redux store, which gets data from the SQLite database via the backend

--- frontend/src/features/annotation/annotationSlice.tsx
+++ frontend/src/features/annotation/annotationSlice.tsx
@@ -7,9 +7,20 @@ type AnnotationState = {
     error: string | null,
 }
 
+// grab annotations from the backend
 export const fetchAnnotations = createAsyncThunk('annotations/fetch', async () => {
-    const response = await fetch('/api/annotations');
-    return // response.data;
+    try {    
+        const response = await fetch('http://localhost:3001/annotations');
+        if (!response.ok) {
+            throw new Error(`Network response was not ok: ${response.status} ${response.statusText}`);
+        }
+        console.log('response', response);
+        const data = await response.json();
+        console.log('data', data);
+        return data;
+    } catch (error) {
+        console.error("Error fetching annotations:", error);
+    }
 });
 
 // Initial state
@@ -31,7 +42,17 @@ const annotationSlice = createSlice({
         }
     },
     extraReducers: (builder) => {
-
+        builder.addCase(fetchAnnotations.pending, (state) => {
+            state.status = 'loading';
+        });
+        builder.addCase(fetchAnnotations.fulfilled, (state, action) => {
+            state.status = 'succeeded';
+            state.list = state.list.concat(action.payload);
+        });
+        builder.addCase(fetchAnnotations.rejected, (state, action) => {
+            state.status = 'failed';
+            state.error = action.error.message ? action.error.message : null;
+        });
     }
 })
 
