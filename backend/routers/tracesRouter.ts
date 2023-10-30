50471ee167bac286841dd6c3f6d70848b01574da
Rob Mayo <rbrtm984@gmail.com>
2023-10-30 15:17:57 -0400

    uploading changes. trying to decode binary data using proto files in otel/proto

--- backend/routers/tracesRouter.ts
+++ backend/routers/tracesRouter.ts
@@ -1,11 +1,73 @@
 const { Router } = require('express');
+import express, { Request, Response, NextFunction } from 'express';
+import protobuf from 'protobufjs';
+import zlib from 'zlib';
 // const tracesController = require('../controllers/tracesController.ts');
-const router = Router()
-import { Request, Response } from 'express'
 
-router.post('*',  (req: Request, res: Response) => {
-    console.log("Received data at /v1/traces:", req.body);
-    res.status(200).send('Trace data received!');
+const traceDescriptor = require("../trace.json")
+const root = protobuf.Root.fromJSON(traceDescriptor);
+const ExportTraceServiceRequest = root.lookupType("opentelemetry.proto.collector.trace.v1.ExportTraceServiceRequest")
+
+function decompressRequest(req: Request, res: Response, next: NextFunction) {
+    // If the content-encoding is not gzip, proceed without decompression
+    if (req.headers['content-encoding'] !== 'gzip') {
+        return next();
+    }
+
+    // Get the compressed data from the request
+    const compressedData: Buffer[] = [];
+
+    req.on('data', (chunk) => {
+        compressedData.push(chunk);
+    });
+
+    req.on('end', () => {
+        const buffer = Buffer.concat(compressedData);
+        
+        zlib.gunzip(buffer, (err, decompressedData) => {
+            if (err) {
+                console.error("Error decompressing data:", err);
+                return res.status(500).send("Error decompressing data");
+            }
+
+            req.body = decompressedData;
+            next();
+        });
+    });
+}
+
+function decodeTraceData(req: Request, res: Response, next: NextFunction) {
+    try {
+        const message = ExportTraceServiceRequest.decode(req.body);
+        req.body = ExportTraceServiceRequest.toObject(message, {
+            longs: String,
+            enums: String,
+            bytes: String
+        });
+        next();
+    } catch (error) {
+        console.error("Error decoding trace data:", error);
+        res.status(500).send("Error decoding trace data");
+    }
+}
+
+const router = Router()
+
+// Use the middleware before the /v1/traces route
+router.post('/v1/traces', decompressRequest, (req: Request, res: Response) => {
+    console.log("Decompressed trace data:", req.body);
+    // console.log('req headers', req.headers);
+    res.status(200).send("Data received, decompressed, and decoded successfully");
 });
+
+// router.use('*', (req: Request, res: Response, next: NextFunction) => {
+//     const message = ExportTraceServiceRequest.decode(req.body);
+//     req.body = ExportTraceServiceRequest.toObject(message, {
+//         longs: String,
+//         enums: String,
+//         bytes: String
+//     });
+//     next();
+// });
 
 export default router;
