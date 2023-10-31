18be47b2ee135173784969ae21b5069c9afcc32b
Rob Mayo <rbrtm984@gmail.com>
2023-10-31 19:08:20 -0400

    update

--- backend/middlewares/authenticateJWT.ts
+++ backend/middlewares/authenticateJWT.ts
@@ -5,12 +5,17 @@ import { IRequest } from '../types';  // Adjust the import path accordingly
 export function authenticateJWT(req: IRequest, res: Response, next: NextFunction) {
     const token = req.cookies.jwtToken;
 
+    console.log('token on backend', token);
+
     if (!token) {
         return res.status(401).json({ error: 'Unauthorized' });
     }
+    
+    console.log(process.env.JWT_SECRET!);
 
     jwt.verify(token, process.env.JWT_SECRET!, (err: any, user: any) => {
         if (err) {
+            console.log('JWT auth err', err);
             return res.status(403).json({ error: 'Forbidden' });  // Forbiddenx
         }
 
