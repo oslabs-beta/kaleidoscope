3c13275e42e0047c9dda61e90d759d7aee2c3a74
thalesIII <tommyhalesiii@gmail.com>
2023-10-31 11:59:22 -0400

    incoming data now being stored to tracestore.json

--- backend/routers/tracesRouter.ts
+++ backend/routers/tracesRouter.ts
@@ -1,15 +1,17 @@
 const { Router } = require('express');
 const path = require('path');
+const fs = require('fs');
 import express, { Request, Response, NextFunction } from 'express';
 import * as protobuf from 'protobufjs';
 import zlib from 'zlib';
-// const tracesController = require('../controllers/tracesController.ts');
+const tracesController = require('../controllers/tracesController.ts');
 
 // const traceDescriptor = require("../trace.json")
 // const root = protobuf.Root.fromJSON(traceDescriptor);
 // const ExportTraceServiceRequest = root.lookupType("opentelemetry.proto.collector.trace.v1.ExportTraceServiceRequest")
 
 function decompressRequest(req: Request, res: Response, next: NextFunction) {
+    // console.log('received traces');
     // If the content-encoding is not gzip, proceed without decompression
     if (req.headers['content-encoding'] !== 'gzip') {
         return next();
@@ -63,14 +65,21 @@ async function decodeTraceData(req: Request, res: Response, next: NextFunction) 
 const router = Router()
 
 // Use the middleware before the /v1/traces route
-router.post('/v1/traces', decompressRequest, decodeTraceData, (req: Request, res: Response) => {
+router.post('/v1/traces', 
+    decompressRequest, 
+    decodeTraceData, 
+    tracesController.storeTraces, 
+    (req: Request, res: Response) => {
     // console.log("Decompressed trace data:", req.body);
-    console.log("Decoded data", req.body.decodedData);
-    console.log("Spans", req.body.decodedData.resourceSpans[0].scopeSpans[0]);
-    console.log("Attributes", JSON.stringify(req.body.decodedData.resourceSpans[0].scopeSpans[0].attributes, null, 2));
-
+    // console.log("Decoded data", req.body.decodedData);
+    // console.log("resourceSpans", req.body.decodedData.resourceSpans[0])
+    // console.log("scopeSpans", req.body.decodedData.resourceSpans[0].scopeSpans[0]);
+    // console.log("a single span", req.body.decodedData.resourceSpans[0].scopeSpans[0].spans[0]);
+    // const l = req.body.decodedData.resourceSpans[0].scopeSpans[0].spans.length - 1;
+    // console.log("Attributes", req.body.decodedData.resourceSpans[0].scopeSpans[0].spans[l].attributes);
 
     // console.log('req headers', req.headers);
+    console.log("Data received, decompressed, and decoded successfully");
     res.status(200).send("Data received, decompressed, and decoded successfully");
 });
 
