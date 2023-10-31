f6d99a6d15d5c3414aed31cea25c3a5870a49db5
Rob Mayo <rbrtm984@gmail.com>
2023-10-31 16:30:45 -0400

    finished setting up auth. implemented JWT for cookies, configured protected routes to verify user has JWT, created authenticate JWT middleware to confirm token, installed various packages, added .env var for JWT

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NewHome from './components/Home/NewHome';
-import ViewCluster from './components/ViewCluster';
 
 function App() {
   return (
