139d46d4aa2f51e79128884c2044f8c50463e286
Rob Mayo <rbrtm984@gmail.com>
2023-11-01 14:55:19 -0400

    updating test config. still doesnt work. but closer

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
     transform: {
       '^.+\\.tsx?$': 'babel-jest',
     },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+    testRegex: '(/__tests__/.*|\\btest_.*|(\\.|/)(test|spec))\\.tsx?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    moduleNameMapper: {
+      "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js"
+    }
   };
